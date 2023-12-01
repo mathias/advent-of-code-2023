b9e55814579181cc8f7c675f6f58c4efff81a3eb
Matt Gauger <matt.gauger@gmail.com>
2023-12-01 16:43:18 -0600

    Working exercise 1 answer

--- 1/day1/src/main.rs
+++ 1/day1/src/main.rs
@@ -1,13 +1,21 @@
-use std::env;
 use std::fs;
+use regex::Regex;
 
 fn main() {
+    let re = Regex::new(r"\d").unwrap();
+    //let file_path : String = "../sample_input.txt".to_string();
     let file_path : String = "../input.txt".to_string();
-    let mut result = Vec::new();
+    let mut result = vec![];
 
     for line in fs::read_to_string(file_path).unwrap().lines() {
-        result.push(line.to_string());
+        let caps : Vec<&str> = re.find_iter(line).map(|m| m.as_str()).collect();
+        let first_digit : i32 = caps.first().unwrap().parse().unwrap();
+        let last_digit : i32 = caps.last().unwrap().parse().unwrap();
+
+        result.push(first_digit * 10 + last_digit);
     }
 
-    print!("{:#?}", result);
+    let sum : i32 = result.into_iter().reduce(|acc, e| acc + e).unwrap();
+
+    println!("{}", sum);
 }
