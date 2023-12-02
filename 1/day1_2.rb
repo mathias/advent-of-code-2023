fd36fd7c445bb37d59639d3b1a9ce93140e496d4
Matt Gauger <matt.gauger@gmail.com>
2023-12-02 13:37:29 -0600

    Switch to class to allow unit testing

--- 1/day1_2.rb
+++ 1/day1_2.rb
@@ -1,62 +1,69 @@
 require "pry"
 
-LINE_REGEX = /([1-9]|one|two|three|four|five|six|seven|eight|nine)/
+class Day
+  LINE_REGEX = /(one|two|three|four|five|six|seven|eight|nine|[1-9])/
 
-def read(path)
-  lines = []
-  File.open(path, "r") do |f|
-      f.each_line do |line|
-        lines << line
-      end
+  def initialize(path)
+    @path = path
   end
-  lines
-end
 
-def parse(lines)
-  results = []
-  lines.each do |line|
-    matches = line.chomp.scan(LINE_REGEX)
-
-    first = parse_word(matches[0][0])
-    last = parse_word(matches[-1][0])
-
-    raise if first < 1 || first > 9 || last < 0 || last > 9
-
-    results << (first * 10 + last)
+  def run
+    parse(read(@path)).sum
   end
-  results
-end
 
-def parse_word(word)
-  case word
-    when "one"
-      1
-    when "two"
-      2
-    when "three"
-      3
-    when "four"
-      4
-    when "five"
-      5
-    when "six"
-      6
-    when "seven"
-      7
-    when "eight"
-      8
-    when "nine"
-      9
-    else
-      word.to_i
+  def read(path)
+    lines = []
+    File.open(path, "r") do |f|
+        f.each_line do |line|
+          lines << line
+        end
+    end
+    lines
+  end
+
+  def parse(lines)
+    lines.map do |line|
+      matches = line.chomp.scan(LINE_REGEX)
+
+      first = parse_word(matches[0][0])
+      last = parse_word(matches[-1][0])
+
+      raise if (first < 1 || first > 9 || last < 1 || last > 9)
+      raise unless first.is_a?(Integer) && last.is_a?(Integer)
+
+      (first * 10 + last)
+    end
+  end
+
+  def parse_word(word)
+    case word
+      when "one"
+        1
+      when "two"
+        2
+      when "three"
+        3
+      when "four"
+        4
+      when "five"
+        5
+      when "six"
+        6
+      when "seven"
+        7
+      when "eight"
+        8
+      when "nine"
+        9
+      else
+        word.to_i
+    end
   end
 end
 
-def add_numbers(list)
-  list.reduce(0) { |acc, n| acc + n }
+# Only run the following code when this file is the main file being run
+# instead of having been required or loaded by another file
+if __FILE__==$0
+  puts Day.new("sample_input_2.txt").run
+  puts Day.new("input.txt").run
 end
-
-parse(read("input.txt")).each { |n| raise "incorrect number: #{n}" if n > 99 || n < 11 }
-
-puts add_numbers(parse(read("sample_input_2.txt")))
-puts add_numbers(parse(read("input.txt")))
