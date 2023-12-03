b5ecb416f632e473e123138208824b985e2287bd
Matt Gauger <matt.gauger@gmail.com>
2023-12-02 20:18:21 -0600

    Clean up and add computing part 2 answer in existing loops

--- 2/day2/src/main.rs
+++ 2/day2/src/main.rs
@@ -12,17 +12,15 @@ fn main() {
     let file_path : String = "input.txt".to_string();
 
     let mut possible_game_ids = vec![];
+    let mut powers = vec![];
+
     possible_game_ids.push(0i32);
+    powers.push(0i32);
 
     for line in fs::read_to_string(file_path).unwrap().lines() {
-        //input.push(line)
-        // parse line into a game
-        // determine if game can be possible with BAG
-        // push onto possible_game_ids if possible
         let (first, rest) = line.split_once (":").unwrap();
         let (_game, id) = first.split_once(" ").unwrap();
 
-        println!("Game #{}", id);
         let (mut red, mut green, mut blue) = (0i32, 0i32, 0i32);
 
         for draw in rest.split(";") {
@@ -39,14 +37,15 @@ fn main() {
             }
 
         }
-        println!("{} {} {}", red, green, blue);
-
         if red <= BAG.red && green <= BAG.green && blue <= BAG.blue {
             possible_game_ids.push(id.parse().unwrap());
         }
 
+        powers.push(red * green * blue);
     }
 
     // add up possible_game_ids and print it:
-    println!("{}", possible_game_ids.into_iter().sum::<i32>());
+    println!("Part 1 answer: {}", possible_game_ids.into_iter().sum::<i32>());
+
+    println!("Part 2 answer: {}", powers.into_iter().sum::<i32>());
 }
